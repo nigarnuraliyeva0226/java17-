73a931aaf022bb9d27c57f7fedaab55921dcb882
nigarnuraliyeva0226 <nikanurali0226@gmail.com>
2023-08-27 16:13:26 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector(`h1`)
+console.log (h1)
+h1.innerHTML= "GeForce 20 Series"
+let ul = document.querySelector(`ul`)
+console.log (ul)
+let li = document.querySelector(`li`)
+console.log (li)
